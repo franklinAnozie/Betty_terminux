a0bb3d61998f5502c82fdfa9df68b95ec6ac1f78
Franklin Anozie <122824019+franklinAnozie@users.noreply.github.com>
2023-10-05 15:07:05 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,3 +8,46 @@ else
     pref="${PREFIX}"
     echo $pref
 fi
+
+BETTY_STYLE="betty-style"
+BETTY_DOC="betty-doc"
+BETTY_WRAPPER="betty"
+
+APP_PATH="${pref}/usr/betty"
+BIN_PATH="${pref}/usr/bin"
+MAN_PATH="${pref}/usr/man/man1"
+
+echo -e "Installing binaries.."
+
+mkdir -pv "${APP_PATH}"
+
+cp "${BETTY_STYLE}.pl" "${APP_PATH}/${BETTY_STYLE}"
+cp "${BETTY_DOC}.pl" "${APP_PATH}/${BETTY_DOC}"
+cp "${BETTY_WRAPPER}.sh" "${APP_PATH}/${BETTY_WRAPPER}"
+
+chmod +x "${APP_PATH}/${BETTY_STYLE}"
+chmod +x "${APP_PATH}/${BETTY_DOC}"
+chmod +x "${APP_PATH}/${BETTY_WRAPPER}"
+
+ln -s "${APP_PATH}/${BETTY_STYLE}" "${BIN_PATH}/${BETTY_STYLE}"
+ln -s "${APP_PATH}/${BETTY_DOC}" "${BIN_PATH}/${BETTY_DOC}"
+ln -s "${APP_PATH}/${BETTY_WRAPPER}" "${BIN_PATH}/${BETTY_WRAPPER}"
+
+echo -e "Installing man pages.."
+
+mkdir -p "${MAN_PATH}"
+
+cp "man/betty.1" "${MAN_PATH}"
+cp "man/${BETTY_STYLE}.1" "${MAN_PATH}"
+cp "man/${BETTY_DOC}.1" "${MAN_PATH}"
+
+echo -e "Updating man database.."
+
+mandb
+
+echo -e "Installing perl.."
+
+pkg install perl
+
+echo -e "All set."
+
